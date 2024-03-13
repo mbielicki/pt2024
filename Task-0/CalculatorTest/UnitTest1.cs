40825e727f83fd343d3e484927989e909490a364
Michał Bielicki <mbielicki2912@gmail.com>
2024-03-13 18:06:57 +0100

    test dividing by zero

--- Task-0/CalculatorTest/UnitTest1.cs
+++ Task-0/CalculatorTest/UnitTest1.cs
@@ -11,24 +11,37 @@ namespace CalculatorTest
             Calculator calculator = new Calculator(); 
             int result = calculator.add(2, 2);
             Assert.AreEqual(4, result);
-        }  
+        }
+
+        [TestMethod]
         public void TestRemove()
         {
             Calculator calculator = new Calculator(); 
             int result = calculator.remove(4, 2);
             Assert.AreEqual(2, result);
-        }   
+        }
+
+        [TestMethod]
         public void TestMultiply()
         {
             Calculator calculator = new Calculator(); 
             int result = calculator.multiply(4, 2);
             Assert.AreEqual(8, result);
-        }   
+        }
+
+        [TestMethod]
         public void TestDivide()
         {
             Calculator calculator = new Calculator(); 
             int result = calculator.divide(6, 2);
             Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestDivideByZero()
+        {
+            Calculator calculator = new Calculator();
+            Assert.ThrowsException<ArithmeticException>(() => calculator.divide(6, 0));
         }
     }
 }

23314c1537656c7456f2cb4d814660f00c304d61
Michał Bielicki <mbielicki2912@gmail.com>
2024-03-13 18:03:54 +0100

    fix divideByZeroException

--- Task-0/Task-0/Calculator.cs
+++ Task-0/Task-0/Calculator.cs
@@ -2,13 +2,19 @@
 {
     public class Calculator
     {
-        public int add(int x, int y) { return x + y; }
-        public int remove(int x, int y) { return x - y; }
+        public int add(int x, int y) { 
+            return x + y; 
+        }
+        public int remove(int x, int y) { 
+            return x - y; 
+        }
         public int multiply(int x, int y) {
-            if (y == 0)
-             throw new ArithmeticException("You can't divide by zero!"); 
             return x * y; 
         }
-        public int divide(int x, int y) { return x / y; }
+        public int divide(int x, int y) {
+            if (y == 0)
+                throw new ArithmeticException("You can't divide by zero!"); 
+            return x / y; 
+        }
     }
 }

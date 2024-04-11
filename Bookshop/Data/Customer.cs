d52393f77c36b86232a5f35e027fc8232fa83f02
gabriela-j <gabriela.jung8@gmail.com>
2024-04-11 17:49:17 +0200

    add Customer class

---
+++ Bookshop/Data/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace Bookshop.Data
+{
+    public class Customer
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string? Address { get; private set; }
+
+        public string? ContactInfo { get; private set; }
+
+        public Customer(int id, string firstName, string lastName, string? address, string? contactInfo)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            ContactInfo = contactInfo;
+        }
+    }
+}
---
+++ BookshopTest/DataTest/CustomerTest.cs
@@ -0,0 +1,25 @@
+﻿using Bookshop.Data;
+
+namespace BookshopTest.DataTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void testCustomerInit()
+        {
+            int id = 0;
+            string firstName = "John";
+            string lastName = "Doe";
+            string address = "00000 Baker Street 221B, London, England";
+            string? contactInfo = null;
+
+            Customer customer = new Customer(id, firstName, lastName, address, contactInfo);
+            Assert.AreEqual(id, customer.Id);
+            Assert.AreEqual(firstName, customer.FirstName);
+            Assert.AreEqual(lastName, customer.LastName);
+            Assert.AreEqual(address, customer.Address);
+            Assert.IsNull(customer.ContactInfo);
+        }
+    }
+}

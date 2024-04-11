7b2c3a7f2fdfef5efa3f5ae7c8a424621d666c62
Michał Bielicki <mbielicki2912@gmail.com>
2024-04-11 17:09:40 +0200

    add Book class

---
+++ Bookshop/Data/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.Data
+{
+    public class Book
+    {
+        public int Id {  get; private set; }
+        public string Name { get; private set; }
+
+        public string Author { get; private set; }
+        public string Description { get; private set; }
+        public double Price { get; private set; }
+
+        public Book(int Id, string Name, string Author, string Description, double Price) {
+            this.Id = Id;
+            this.Name = Name;
+            this.Author = Author;
+            this.Description = Description;
+            this.Price = Price;
+        }
+    }
+}
---
+++ BookshopTest/DataTest/BookTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookshop.Data;
+
+namespace BookshopTest
+{
+    [TestClass]
+    public class BookTest
+    {
+        [TestMethod]
+        public void testBookInit()
+        {
+            int id = 0;
+            string name = "Pan Tadeusz";
+            string author = "Adam Mickiewicz";
+            string description = "The Last Foray in Lithuania";
+            double price = 10;
+            Book book = new Book(id, name, author, description, price);
+
+            Assert.AreEqual(book.Id, id);
+            Assert.AreEqual(book.Name, name);
+            Assert.AreEqual(book.Author, author);
+            Assert.AreEqual(book.Description, description);
+            Assert.AreEqual(book.Price, price);
+        }
+        
+    }
+}

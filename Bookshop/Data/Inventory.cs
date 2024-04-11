fec73a80de6cf13469965ed68fb2aa40cb9fbb27
Michał Bielicki <mbielicki2912@gmail.com>
2024-04-11 19:50:36 +0200

    add Inventory class

---
+++ Bookshop/Data/Inventory.cs
@@ -0,0 +1,43 @@
+﻿namespace Bookshop.Data
+{
+    public class Inventory
+    {
+        private Dictionary<Book, int> inventory;
+
+        public Inventory() {
+            inventory = new Dictionary<Book, int>();
+        }
+
+        public void Add(Book book)
+        {
+            if (inventory.ContainsKey(book))
+                inventory[book]++;
+            else
+                inventory.Add(book, 1);
+        }
+        public void Remove(Book book)
+        {
+            if (inventory.ContainsKey(book))
+            {
+                int newCount = inventory[book] - 1;
+                if (newCount > 0)
+                    inventory[book] = newCount;
+                else inventory.Remove(book);
+
+            }
+                
+            else throw new NoSuchBookInInventory();
+        }
+
+        public int Count(Book book)
+        {
+            if (!inventory.ContainsKey(book)) return 0;
+            return inventory[book];
+        }
+    }
+
+    public class NoSuchBookInInventory : Exception 
+    {
+
+    }
+}
---
+++ BookshopTest/DataTest/InventoryTest.cs
@@ -0,0 +1,35 @@
+﻿using Bookshop.Data;
+
+namespace BookshopTest.DataTest
+{
+    [TestClass]
+    public class InventoryTest
+    {
+        [TestMethod]
+        public void testInventory()
+        {
+            int bookId = 321;
+            string name = "Pan Tadeusz";
+            string author = "Adam Mickiewicz";
+            string description = "The Last Foray in Lithuania";
+            double price = 10;
+            Book book = new Book(bookId, name, author, description, price);
+
+            Inventory inventory = new Inventory();
+            Assert.AreEqual(0, inventory.Count(book));
+
+            inventory.Add(book);
+            Assert.AreEqual(1, inventory.Count(book));
+
+            inventory.Add(book);
+            Assert.AreEqual(2, inventory.Count(book));
+
+            inventory.Remove(book);
+            Assert.AreEqual(1, inventory.Count(book));
+
+            inventory.Remove(book);
+            Assert.ThrowsException<NoSuchBookInInventory>(() => inventory.Remove(book));
+
+        }
+    }
+}

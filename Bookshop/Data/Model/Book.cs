b8752bf71a99832ec21b314d89d1320d8ed9c710
Michał Bielicki <mbielicki2912@gmail.com>
2024-04-12 01:00:39 +0200

    change Book model properties to nullable

--- Bookshop/Data/Model/Book.cs
+++ Bookshop/Data/Model/Book.cs
@@ -3,33 +3,18 @@
     public class Book
     {
         public int? Id { get; set; }
-        public string Name { get; private set; }
-        public string Author { get; set; }
-        public string Description { get; set; }
-        public double Price { get; set; }
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
 
-        public Book(int Id, string Name, string Author, string Description, double Price)
+        public Book(int? Id = null, string? Name = null, string? Author = null, string? Description = null, double? Price = null)
         {
             this.Id = Id;
             this.Name = Name;
             this.Author = Author;
             this.Description = Description;
             this.Price = Price;
-        }
-        public Book(string Name, string Author, string Description, double Price)
-        {
-            Id = null;
-            this.Name = Name;
-            this.Author = Author;
-            this.Description = Description;
-            this.Price = Price;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (obj == null || !(obj is Book))
-                return false;
-            return Id == ((Book)obj).Id;
         }
     }
 }

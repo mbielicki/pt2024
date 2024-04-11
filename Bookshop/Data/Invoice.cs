4a56a0e1b862a9c3040431bebb4eaa9d99976002
gabriela-j <gabriela.jung8@gmail.com>
2024-04-11 19:09:29 +0200

    add Invoice class

---
+++ Bookshop/Data/Invoice.cs
@@ -0,0 +1,26 @@
+﻿namespace Bookshop.Data
+{
+    public class NoBooksInInvoice : Exception
+    {
+
+    }
+    public class Invoice
+    {
+        public int Id { get; private set; }
+        public List<Book> Books { get; private set; }
+        public Customer Customer { get; private set; }
+        public double Price { get; private set; }
+        public DateTime DateTime { get; private set; }
+        public Invoice(int id, List<Book> books, Customer customer, double price, DateTime dateTime)
+        {
+            if (books.Count == 0)
+                throw new NoBooksInInvoice();
+
+            Id = id;
+            Books = books;
+            Customer = customer;
+            Price = price;
+            DateTime = dateTime;
+        }
+    }
+}
---
+++ BookshopTest/DataTest/InvoiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Bookshop.Data;
+
+namespace BookshopTest.DataTest
+{
+    [TestClass]
+    public class InvoiceTest
+    {
+        [TestMethod]
+        public void testInvoiceInit()
+        {
+            int customerId = 123;
+            string firstName = "John";
+            string lastName = "Doe";
+            string address = "00000 Baker Street 221B, London, England";
+            string? contactInfo = null;
+            Customer customer = new Customer(customerId, firstName, lastName, address, contactInfo);
+
+            int bookId = 321;
+            string name = "Pan Tadeusz";
+            string author = "Adam Mickiewicz";
+            string description = "The Last Foray in Lithuania";
+            double price = 10;
+            Book book = new Book(bookId, name, author, description, price);
+
+            int invoiceId = 456;
+            List<Book> books = new List<Book>();
+            DateTime now = DateTime.Now;
+
+            Assert.ThrowsException<NoBooksInInvoice>(() => new Invoice(invoiceId, books, customer, price, now));
+
+            books.Add(book);
+            Invoice invoice = new Invoice(invoiceId, books, customer, price, now);
+
+            Assert.AreEqual(invoiceId, invoice.Id);
+            Assert.AreEqual(customerId, invoice.Customer.Id);
+            Assert.AreEqual(book.Name, invoice.Books[0].Name);
+            Assert.AreEqual(price, invoice.Price);
+            Assert.AreEqual(now, invoice.DateTime);
+        }
+    }
+}

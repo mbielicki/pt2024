5739ce750fcf34242c245f2d8a504c08b050b259
Michał Bielicki <mbielicki2912@gmail.com>
2024-04-12 01:23:11 +0200

    add testUpdate to InMemoryStorageTest

--- BookshopTest/DataTest/InMemoryStorageTest.cs
+++ BookshopTest/DataTest/InMemoryStorageTest.cs
@@ -7,7 +7,7 @@ namespace BookshopTest.DataTest
     public class InMemoryStorageTest
     {
         [TestMethod]
-        public void AddGet()
+        public void testAddGet()
         {
             int bookId = 321;
             string name = "Pan Tadeusz";
@@ -18,6 +18,41 @@ namespace BookshopTest.DataTest
 
             IStorage storage = new InMemoryStorage();
             storage.add(book);
+            Assert.AreEqual(book, storage.get(bookId));
+        }
+        
+        [TestMethod]
+        public void testRemove()
+        {
+            int bookId = 321;
+            string name = "Pan Tadeusz";
+            string author = "Adam Mickiewicz";
+            string description = "The Last Foray in Lithuania";
+            double price = 10;
+            Book book = new Book(bookId, name, author, description, price);
+
+            IStorage storage = new InMemoryStorage();
+            storage.add(book);
+            storage.remove(bookId);
+            Assert.IsNull(storage.get(bookId));
+        }
+
+        [TestMethod]
+        public void testUpdate()
+        {
+            int bookId = 321;
+            string name = "Pan Tadeusz";
+            string author = "Adam Mickiewicz";
+            string description = "The Last Foray in Lithuania";
+            double price = 10;
+            Book book = new Book(bookId, name, author, description, price);
+
+            IStorage storage = new InMemoryStorage();
+            storage.add(book);
+
+            double newPrice = 20;
+            Book newBook = new Book(bookId, name, author, description, newPrice);
+            storage.update(newBook);
             Assert.AreEqual(book, storage.get(bookId));
         }
     }
